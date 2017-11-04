85f1ebc5c399050e3e839063259309113a84b939
Daniel Rudolf <github.com@daniel-rudolf.de>
2017-11-04 18:25:32 +0100

    Add license/copyright file header to index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<?php // @codingStandardsIgnoreFile
+/**
+ * This file is part of Pico. It's copyrighted by the contributors recorded
+ * in the version control history of the file, available from the following
+ * original location:
+ *
+ * <https://github.com/picocms/pico-composer/blob/master/index.php>
+ *
+ * SPDX-License-Identifier: MIT
+ * License-Filename: LICENSE
+ */
 
 // load dependencies
 // pico-composer MUST be installed as root package
